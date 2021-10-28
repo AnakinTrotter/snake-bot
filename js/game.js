c3a1f38cbcdc2f035534c89105880fbb3778c1df
Anakin Trotter <anakin.trotter@gmail.com>
2021-10-28 01:45:30 -0700

    Added cycle following
    Snake can calculate a Hamiltonian cycle and follow it to victory. Also added two play buttons so user can choose to play normally, or see the computer play.

--- js/game.js
+++ js/game.js
@@ -3,15 +3,17 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 // initialize grid
-const TILE_SIZE = 20;
+const TILE_SIZE = 40;
 const m = canvas.width / TILE_SIZE;
 const n = canvas.height / TILE_SIZE;
 const grid = new Array(m);
 for (let i = 0; i < m; i++) {
-    grid[i] = new Array(n);
+    grid[i] = new Array(n).fill(0);
 }
 
 // handle controls
+var humanPlaying = true;
+var interval = 0;
 const dirs = {
     left: [-1, 0],
     right: [1, 0],
@@ -20,7 +22,7 @@ const dirs = {
 }
 var moveQueue = []
 document.addEventListener("keydown", function (event) {
-    if(moveQueue.length >= 2) {
+    if (moveQueue.length >= 2) {
         return;
     }
     moveQueue.push(event);
@@ -40,10 +42,52 @@ function handleInput() {
         snake.dir = dirs.down;
     }
 }
+function followCycle() {
+    let sx = snake.body[0][0];
+    let sy = snake.body[0][1];
+    snake.dir = cycle[sx][sy];
+}
+
+// calculate hamiltonian cycle
+const cycle = new Array(m);
+for (let i = 0; i < m; i++) {
+    cycle[i] = new Array(n).fill(0);
+}
+function isValid(x, y) {
+    if (x < 0 || x >= m || y < 0 || y >= n) {
+        return false;
+    }
+    return cycle[x][y] == 0;
+}
+function findCycle(x, y, depth) {
+    if (depth == m * n - 1) {
+        for (let dir in dirs) {
+            let x1 = x + dirs[dir][0];
+            let y1 = y + dirs[dir][1];
+            if (x1 == 0 && y1 == 0) {
+                cycle[x][y] = dirs[dir];
+                return true;
+            }
+        }
+        return false;
+    }
+    for (let dir in dirs) {
+        let x1 = x + dirs[dir][0];
+        let y1 = y + dirs[dir][1];
+        if (isValid(x1, y1)) {
+            cycle[x][y] = dirs[dir];
+            if (findCycle(x1, y1, depth + 1)) {
+                return true;
+            }
+            cycle[x][y] = 0;
+        }
+    }
+    return false;
+}
 
 // initialize snake
 const snake = {
-    body: [[m / 2, n / 2], [m / 2, n / 2 + 1], [m / 2, n / 2 + 2]],
+    body: [[Math.floor(m / 2), Math.floor(n / 2)], [Math.floor(m / 2), Math.floor(n / 2 + 1)]],
     dir: dirs.up
 };
 for (let i = 0; i < snake.body.length; i++) {
@@ -60,6 +104,13 @@ grid[apple.x][apple.y] = 1;
 // GAME LOGIC
 
 function moveSnake() {
+    if(snake.body.length >= m * n) {
+        alert("YOU WIN!");
+        document.location.reload();
+        clearInterval(interval); // Needed for Chrome to end game
+        console.log("WIN!");
+        return;
+    }
     let newHead = [];
     let dir = [snake.dir[0], snake.dir[1]];
     newHead[0] = snake.body[0][0] + dir[0];
@@ -69,7 +120,8 @@ function moveSnake() {
         document.location.reload();
         clearInterval(interval); // Needed for Chrome to end game
         console.log("OUT");
-    } else if (grid[newHead[0]][newHead[1]] == 1) {
+        return;
+    } else if (newHead[0] == apple.x && newHead[1] == apple.y) {
         eatApple();
         console.log("YUM");
     } else if (grid[newHead[0]][newHead[1]] == 2) {
@@ -77,6 +129,7 @@ function moveSnake() {
         document.location.reload();
         clearInterval(interval); // Needed for Chrome to end game
         console.log("HIT");
+        return;
     }
     snake.body.unshift(newHead);
     let oldTail = snake.body.pop();
@@ -87,7 +140,7 @@ function moveSnake() {
 function eatApple() {
     let xa = Math.floor(Math.random() * m);
     let ya = Math.floor(Math.random() * n);
-    while (grid[xa][ya] == 2) {
+    while (grid[xa][ya] != 0) {
         xa = Math.floor(Math.random() * m)
         ya = Math.floor(Math.random() * n)
     }
@@ -95,8 +148,10 @@ function eatApple() {
     apple.y = ya;
     grid[apple.x][apple.y] = 1;
     let oldTail = snake.body[snake.body.length - 1];
-    let xs = oldTail[0] - snake.dir[0];
-    let ys = oldTail[1] - snake.dir[1];
+    let olderTail = snake.body[snake.body.length - 2];
+    let tailDir = [olderTail[0] - oldTail[0], olderTail[1] - oldTail[1]];
+    let xs = oldTail[0] + tailDir[0];
+    let ys = oldTail[1] + tailDir[1];
     snake.body.push([xs, ys]);
 }
 
@@ -129,10 +184,31 @@ function draw() {
 
 // MAIN GAME LOOP
 
+const GAME_SPEED = 150;
 function play() {
-    handleInput();
-    moveSnake();
+    if(humanPlaying) {
+        handleInput();
+    } else {
+        followCycle();
+    }
+    moveSnake(); 
     draw();
 }
-var interval = setInterval(play, 100);
 
+// BUTTON LOGIC
+function playComputer() {
+    //document.location.reload();
+    humanPlaying = false;
+    findCycle(0, 0, 0);
+    interval = setInterval(play, GAME_SPEED);
+}
+
+function playHuman() {
+    //document.location.reload();
+    clearInterval(interval);
+    humanPlaying = true;
+    interval = setInterval(play, GAME_SPEED);
+}
+
+ctx.font = '1em serif';
+ctx.fillText("Press play button to start!", 40, 90);
