c23fe24eaf3335e2cead03aa33eefd0275cd088e
Anakin Trotter <anakin.trotter@gmail.com>
2021-11-11 00:20:55 -0800

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ function auto_version($file)
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Snake Bot</title>
     <link rel="icon" href="img/icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
