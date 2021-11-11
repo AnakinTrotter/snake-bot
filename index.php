3d0ebd07eec20087d6efd76207399f2488fa8dd1
Anakin Trotter <anakin.trotter@gmail.com>
2021-11-11 00:19:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -24,10 +24,11 @@ function auto_version($file)
 
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Snake Bot</title>
     <link rel="icon" href="img/icon.png">
-    <link rel="stylesheet" href="<?php echo auto_version('css/main.css'); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo auto_version('css/main.css'); ?>">
 </head>
 
 <body>
@@ -201,8 +202,8 @@ function findCycle(x, y, depth) {
                 </ol>
             </div>
         </div>
-        <script src="<?php echo auto_version('js/game.js'); ?>"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="<?php echo auto_version('js/game.js'); ?>"></script>
 </body>
 </div>
 
